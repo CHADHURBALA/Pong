75fbe0f4daf0c34da9e649717a8f67730538532a
CHADHURBALA R V <75218512+CHADHURBALA@users.noreply.github.com>
2023-03-11 19:51:43 +0530

    add paddle

---
+++ paddle.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+UP = 90
+DOWN = 270
+
+
+class Paddle(Turtle):
+    def __init__(self, cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(cor)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
